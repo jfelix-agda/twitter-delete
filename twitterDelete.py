22a86820b31979b209e0a7fc82cee852f185934e
John Felix Agda <john.agda@gmail.com>
2022-12-15 22:18:58 +0800

    First commit. A Python Script to remove tweets from today up to one year ago.

---
+++ twitterDelete.py
@@ -0,0 +1,51 @@
+# This program deletes all tweets up until 1 year ago from today.
+# Created by: John Felix Agda
+
+from datetime import datetime, timedelta
+from xml.etree.ElementTree import tostring
+import tweepy
+import configparser
+import time
+import os
+
+# Reads config.ini
+config = configparser.RawConfigParser()
+config.read('config.ini')
+
+conffile = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.ini')
+config.read(conffile)
+
+apiKey = config['twitterInfo']['apiKey']
+apiKeySecret = config['twitterInfo']['apiKeySecret']
+bearerToken = config['twitterInfo']['bearerToken']
+
+accessToken = config['twitterInfo']['accessToken']
+accessTokenSecret = config['twitterInfo']['accessTokenSecret']
+
+# Contains my personal user ID
+userID = config['twitterInfo']['userID']
+
+# Authentication
+client = tweepy.Client(
+    consumer_key= apiKey,
+    consumer_secret= apiKeySecret,
+    access_token= accessToken,
+    access_token_secret= accessTokenSecret,
+    bearer_token=bearerToken
+)
+
+# Calculates the date 1 year ago from today
+currentDate = datetime.today()
+minDate = datetime.today() - timedelta(days=365)
+
+# Queries the list of tweets
+tweetPaginator = tweepy.Paginator(client.get_users_tweets,userID,max_results=100,start_time=minDate, end_time=currentDate)
+
+# Deletes the tweets
+for tweet in tweetPaginator.flatten():
+    print("Deleting Tweet:")
+    print(tweet.id)
+    #client.delete_tweet(tweet.id)
+    time.sleep(0.5)
+
+
